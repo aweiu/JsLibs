7cee443c45932fffbce3c537ea04caac02060e76
awei <328080339@qq.com>
2016-08-19 14:09:05 +0800

    重构+优化 很久之前更新的了，一直没有提交，因为没有真ie8测试。之后不再打算更新了，所以还是上传了吧，有问题可以提issue 更新了哪些东西我有些记不得了。。 最重要的更新是input事件的模拟会忽略js对值的改变，也就是说和现代浏览器完全保持一致

--- libs/js-dev/fixJsForIE8.js
+++ libs/js-dev/fixJsForIE8.js
@@ -1,15 +1,14 @@
 /**
  * Created by awei on 2015/12/1.
  */
-window.console = window.console || (function () {
+(function(){
+    window.console = window.console || (function () {
         var c = {};
         c.log = c.warn = c.debug = c.info = c.error = c.time = c.dir = c.profile
-            = c.clear = c.exception = c.trace = c.assert = function () {
-        };
+            = c.clear = c.exception = c.trace = c.assert = function () {};
         return c;
     })();
-if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function (elt /*, from*/) {
+    Array.prototype.indexOf = Array.prototype.indexOf||function (elt /*, from*/) {
         var len = this.length >>> 0;
         var from = Number(arguments[1]) || 0;
         from = (from < 0) ? Math.ceil(from) : Math.floor(from);
@@ -19,147 +18,205 @@ if (!Array.prototype.indexOf) {
         }
         return -1;
     };
-}
-if (!Array.prototype.forEach) {
-    Array.prototype.forEach = function (fun /*, thisp*/) {
-        var len = this.length;
+    Array.prototype.forEach = Array.prototype.forEach||function (fun /*, thisp*/) {
         if (typeof fun != "function")
             throw new TypeError();
         var thisp = arguments[1];
-        for (var i = 0; i < len; i++) {
-            if (i in this)
-                fun.call(thisp, this[i], i, this);
+        for (var i = 0,l=this.length; i < l; i++) {
+            fun.call(thisp, this[i], i, this);
         }
     };
-}
-window.HTMLElement = window.HTMLElement || Element;
-window.HTMLDocument = window.HTMLDocument || Document;
+    window.HTMLElement = window.HTMLElement || Element;
+    window.HTMLDocument = window.HTMLDocument || Document;
+    var eventQueue={
+        add:function(node,type,fuc){
+            var rValue=false;
+            if(!this.hasOwnProperty(node))this[node]={};
+            if(!this[node].hasOwnProperty(type)){
+                this[node][type]=[];
+                rValue=true;
+            }
+            var evtArr=this[node][type];
+            if(evtArr.indexOf(fuc)==-1)evtArr.push(fuc);
+            return rValue;
+        },
+        remove:function(node,type,fuc){
+            try{
+                var evtArr=this[node][type];
+                evtArr.splice(evtArr.indexOf(fuc), 1);
+                if(evtArr.length==0)delete this[node][type];
+            }catch(e){
 
-HTMLElement.prototype.addEventListener = HTMLElement.prototype.addEventListener || function (eventType, fuc) {
-        var expandFuc = function () {
-            return true
-        }, that = this;
-        switch (eventType) {
-            case "input":
-                var onMyPropertychange = function (e) {
-                    var target = e.srcElement;
-                    if (that.eventQueue && that.eventQueue["mousedown"].indexOf(onMyInput) != -1) {
-                        if (e.propertyName == "value" || e.propertyName == "selectedIndex") {
-                            if (!target.isSetValByJs) {
-                                fuc.call(that, e);
-                            }
-                        }
-                    } else {
-                        target.removeEventListener("propertychange", onMyPropertychange);
-                    }
-                };
-                var onMyInput = function (e) {
-                    var target = e.srcElement;
-                    if (target.value != null) {
-                        target.addEventListener("propertychange", onMyPropertychange);
-                    }
-                };
-                if (!this.inputQueue)this.inputQueue = [];
-                this.inputQueue.push(fuc);
-                this.inputQueue.push(onMyInput);
-                this.addEventListener("mousedown", onMyInput);
-                this.addEventListener("keydown", onMyInput);
-                return;
-            case "blur":
-                eventType = "focusout";
-                expandFuc = function (e) {
-                    return document.activeElement != e.srcElement;
-                };
-                break;
-            case "focus":
-                eventType = "focusin";
-                expandFuc = function (e) {
-                    return document.activeElement == e.srcElement;
-                };
-                break;
+            }
+        },
+        get:function(node,type){
+            try{
+                return this[node][type].slice();
+            }catch(e){
+                return [];
+            }
+        },
+        contain:function(node,type,fuc){
+            try {
+                return this[node][type].indexOf(fuc)!=-1;
+            }catch (e){
+                return false;
+            }
         }
-        if (!this.eventQueue)this.eventQueue = {};
-        if (!this.eventQueue[eventType])this.eventQueue[eventType] = [];
-        var eq = this.eventQueue[eventType];
-        if (eq.indexOf(fuc) != -1)return;
-        eq.push(fuc);
-        if (!this["my" + eventType]) {
-            this["my" + eventType] = function (e) {
-                try {
-                    if (expandFuc.call(that, e)) {
-                        var tmp = that.eventQueue[eventType].slice();
-                        tmp.forEach(function (m) {
-                            if (typeof m == "function" && that.eventQueue[eventType].indexOf(m) != -1)m.call(that, e);
-                        })
-                    }
-                } catch (e) {
+    },
+    eventHost={
+        add:function(node,type,fuc,host){
+            if(!this.hasOwnProperty(node))this[node]={};
+            if(!this[node].hasOwnProperty(type))this[node][type]={};
+            if(!this[node][type].hasOwnProperty(fuc))this[node][type][fuc]=[];
+            if(this[node][type][fuc].length==0){
+                this[node][type][fuc].push(host);
+                return true;
+            }
+        },
+        get:function(node,type,fuc){
+            try{
+                return this[node][type][fuc];
+            }catch(e){
+            }
+        },
+        remove:function(node,type,fuc){
+            try{
+                delete this[node][type][fuc];
+            }catch(e){
 
+            }
+        }
+    },
+    specialEvents={
+        input:[
+            "focusin",
+            function(e){
+                return document.activeElement == e.srcElement;
+            }
+        ],
+        blur:[
+            "focusout",
+            function(e){
+                return document.activeElement != e.srcElement;
+            }
+        ],
+        focus:[
+            "focusin",
+            function(e){
+                return document.activeElement == e.srcElement;
+            }
+        ],
+        get:function(eventType){
+            if(this.hasOwnProperty(eventType))return this[eventType];
+            return [];
+        }
+    },
+    setValByJs=false;
+    HTMLElement.prototype.addEventListener = HTMLElement.prototype.addEventListener || function (eventType,fuc) {
+        if(typeof fuc!="function")return;
+        var that = this,
+            specialEvent=specialEvents.get(eventType),
+            realEventType=specialEvent[0]||eventType,
+            realFuc=fuc,
+            eventFilter=specialEvent[1];
+        if(eventType=="input"){
+            var propertychangeToInput=function(e){
+                var target=e.srcElement;
+                if(!eventHost.get(that,"input",fuc)){
+                    target.removeEventListener("propertychange",propertychangeToInput);
+                }else if(e.propertyName == "value" || e.propertyName == "selectedIndex"){
+                    if(!setValByJs){
+                        fuc.call(target,e);
+                    }else{
+                        setTimeout(function(){
+                            setValByJs=false;
+                        },0);
+                    }
                 }
             };
-            this.attachEvent("on" + eventType, this["my" + eventType]);
-        }
-    };
-HTMLDocument.prototype.addEventListener = HTMLDocument.prototype.addEventListener || HTMLElement.prototype.addEventListener;
-window.addEventListener = window.addEventListener || HTMLElement.prototype.addEventListener;
-HTMLElement.prototype.removeEventListener = HTMLElement.prototype.removeEventListener || function (eventType, fuc) {
-        switch (eventType) {
-            case "input":
-                if (this.inputQueue) {
-                    var fucIndex = this.inputQueue.indexOf(fuc);
-                    if (fucIndex == -1)return;
-                    if (this.eventQueue && this.eventQueue["mousedown"]) {
-                        var onMyInput = this.inputQueue[fucIndex + 1];
-                        this.eventQueue["mousedown"].splice(this.eventQueue["mousedown"].indexOf(onMyInput), 1);
-                        this.eventQueue["keydown"].splice(this.eventQueue["keydown"].indexOf(onMyInput), 1);
-                        this.inputQueue.splice(fucIndex, 2);
-                    }
+            realFuc=function(e){
+                var target=e.srcElement;
+                if(target.value != null){
+                    target.addEventListener("propertychange",propertychangeToInput);
+                    Object.defineProperty(target,"value",{
+                        set:function(val){
+                            setValByJs=true;
+                            this.setAttribute("value",val);
+                        },
+                        get:function(){
+                            return this.getAttribute("value");
+                        }
+                    })
                 }
-                return;
-            case "blur":
-                eventType = "focusout";
-                break;
+            }
+            if(!eventHost.add(this,"input",fuc,["focusin",realFuc]))return;
         }
-        if (this.eventQueue && this.eventQueue[eventType]) {
-            this.eventQueue[eventType].splice(this.eventQueue[eventType].indexOf(fuc), 1);
+        if(eventQueue.add(this,realEventType,realFuc)){
+            this.attachEvent("on" + realEventType, function(e){
+                if(!eventFilter||eventFilter(e)){
+                    eventQueue.get(that,realEventType).forEach(function(m){
+                        if(eventQueue.contain(that,realEventType,m)){
+                            m.call(that, e);
+                        }
+                    });
+                }
+            });
         }
     };
-HTMLDocument.prototype.removeEventListener = HTMLDocument.prototype.removeEventListener || HTMLElement.prototype.removeEventListener;
-window.removeEventListener = window.removeEventListener || HTMLElement.prototype.removeEventListener;
-Event.prototype.preventDefault = Event.prototype.preventDefault || function () {
-        this.returnValue = false
-    };
-Event.prototype.stopPropagation = Event.prototype.stopPropagation || function () {
-        this.cancelBubble = true
-    };
-HTMLElement.prototype.getComputedStyle = HTMLElement.prototype.getComputedStyle || function () {
-        return this.currentStyle;
-    };
-var getBoundingClientRectCopy = HTMLElement.prototype.getBoundingClientRect;
-HTMLElement.prototype.getBoundingClientRect = function () {
-    getBoundingClientRectCopy.call(document.body);
-    var rValue=getBoundingClientRectCopy.call(this);
-    return {
-        left:rValue.left,
-        right:rValue.right,
-        top:rValue.top,
-        bottom:rValue.bottom,
-        width:rValue.right-rValue.left,
-        height:rValue.bottom-rValue.top
-    };
-};
-window.getComputedStyle = window.getComputedStyle || function (obj) {
-        var style = {
-            getPropertyValue: function (key) {
-                return this[key]
+    HTMLDocument.prototype.addEventListener = HTMLDocument.prototype.addEventListener || HTMLElement.prototype.addEventListener;
+    window.addEventListener = window.addEventListener || HTMLElement.prototype.addEventListener;
+    HTMLElement.prototype.removeEventListener = HTMLElement.prototype.removeEventListener || function (eventType, fuc) {
+            if(typeof fuc!="function")return;
+            var realEventType=specialEvents.get(eventType)[0]||eventType,
+                realFuc=eventHost.get(this,eventType,fuc)||fuc;
+            if(typeof(realFuc)=="function"){
+                eventQueue.remove(this,realEventType,realFuc)
+            }else{
+                realFuc.forEach(function (m) {
+                    eventQueue.remove(this,m[0],m[1]);
+                });
+                eventHost.remove(this,eventType,fuc);
             }
         };
-        for (var o in obj.currentStyle) {
-            try {
-                style[o] = obj.currentStyle[o];
-            } catch (e) {
-                //console.log("can't support "+o);
-            }
-        }
-        return style;
+    HTMLDocument.prototype.removeEventListener = HTMLDocument.prototype.removeEventListener || HTMLElement.prototype.removeEventListener;
+    window.removeEventListener = window.removeEventListener || HTMLElement.prototype.removeEventListener;
+    Event.prototype.preventDefault = Event.prototype.preventDefault || function () {
+            this.returnValue = false
+        };
+    Event.prototype.stopPropagation = Event.prototype.stopPropagation || function () {
+            this.cancelBubble = true
+        };
+    HTMLElement.prototype.getComputedStyle = HTMLElement.prototype.getComputedStyle || function () {
+            return this.currentStyle;
+        };
+    var getBoundingClientRectCopy = HTMLElement.prototype.getBoundingClientRect;
+    HTMLElement.prototype.getBoundingClientRect = function () {
+        getBoundingClientRectCopy.call(document.body);
+        var rValue=getBoundingClientRectCopy.call(this);
+        return {
+            left:rValue.left,
+            right:rValue.right,
+            top:rValue.top,
+            bottom:rValue.bottom,
+            width:rValue.right-rValue.left,
+            height:rValue.bottom-rValue.top
+        };
     };
-HTMLElement.prototype.hasOwnProperty=Object.prototype.hasOwnProperty;
+    window.getComputedStyle = window.getComputedStyle || function (obj) {
+            var style = {
+                getPropertyValue: function (key) {
+                    return this[key]
+                }
+            };
+            for (var o in obj.currentStyle) {
+                try {
+                    style[o] = obj.currentStyle[o];
+                } catch (e) {
+                    //console.log("can't support "+o);
+                }
+            }
+            return style;
+        };
+    HTMLElement.prototype.hasOwnProperty=Object.prototype.hasOwnProperty;
+})();
